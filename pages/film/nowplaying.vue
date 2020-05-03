ead19a428fbe565e256b5243cecdd645f58f3a47
wangming <1603657728@qq.com>
2020-05-03 16:54:08 +0800

    9 elementui

--- pages/film/nowplaying.vue
+++ pages/film/nowplaying.vue
@@ -6,6 +6,20 @@
                 {{ data.name }}
             </li>
         </ul>
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+
+        <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in 4" :key="item">
+                <h3>{{ item }}</h3>
+            </el-carousel-item>
+        </el-carousel>
     </div>
 </template>
 
@@ -47,5 +61,19 @@
 </script>
 
 <style scoped>
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+    }
 
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
 </style>
