5043cc026242e0881a84854e573d51d43d78b034
wangming <1603657728@qq.com>
2020-05-03 09:52:52 +0800

    7 asyncData-2

--- pages/detail/_myid.vue
+++ pages/detail/_myid.vue
@@ -2,19 +2,34 @@
     <div>
          <!--获取参数 参数名为本文件名 -->
         detail -- {{ this.$route.params.myid }}
+        {{ dataList }}
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         name: "_myid",
         // 指定根组件的模板
         layout: 'detailtemplate',
         data() {
-            return {}
+            return {
+                dataList: null
+            }
         },
         asyncData(data) {
             console.log(data.params)
+            return axios({
+                url: `https://m.maizuo.com/gateway?filmId=${data.params.myid}&k=559072`,
+                headers: {
+                    'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"15880353302293512536366","bc":"440300"}',
+                    'X-Host': 'mall.film-ticket.film.info'
+                }
+            }).then(res => {
+                return {
+                    dataList: res.data.data.films
+                }
+            })
         }
     }
 </script>
